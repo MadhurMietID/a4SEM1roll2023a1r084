dca77c2077dc10ca79d4053be6302fca827bd2ab
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-10-06 11:44:34 +0530

    Add files via upload

---
+++ 06102023pt3.c
@@ -0,0 +1,12 @@
+//check if a number is divisible by 3 if yes then print yes
+#include<stdio.h>
+void main(){
+    int a;
+    printf("enter the number=");
+    scanf("%d",&a);
+    if (a%3==0)
+    {
+        printf("yes");
+    }
+
+}
