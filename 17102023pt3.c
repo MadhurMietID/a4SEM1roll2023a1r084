add467d74eb284cd6da80dc1e710400c51682c18
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-10-17 11:53:04 +0530

    Add files via upload

---
+++ 17102023pt3.c
@@ -0,0 +1,17 @@
+//sum of  first n natural numbers using a loop
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+
+
+    printf("Program is for sum of n natural numbers .Enter a positive integer n.: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
+
