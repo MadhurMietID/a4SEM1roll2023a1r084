02bb50c214c1206fb1dd99960b2f7d4d88006679
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-10-28 11:25:13 +0530

    Add files via upload

---
+++ 25092023pt1.c
@@ -0,0 +1,8 @@
+/*The pre-increment operator (++x) increments the value of the variable before using 
+it in an expression. It first adds one to the variable and then returns the updated value*/
+#include <stdio.h>
+void main(){
+    int i =10;
+    printf("I=%d\n", ++i);
+    printf("I=%d\n",i);
+}
---
+++ 25092023pt4.c
@@ -0,0 +1,7 @@
+/*the post-increment operator (x++) increments the value of the variable after using it in an
+ expression. It returns the original value of the variable and then adds one to it*/
+ void main(){
+    int i =10;
+    printf("I=%d\n", i++);
+    printf("I=%d\n",i);
+}
