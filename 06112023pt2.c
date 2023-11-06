1592c408ebd67c9929c9299b976167266ea66961
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-11-06 14:27:05 +0530

    Add files via upload

---
+++ 06112023pt2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    printf("enter the values in the array");
+}
+void main()
+{
+    display();
+    extern int x;
+    x = x + x;
+    printf("extern pgm x=%d",x);
+    //x=x-x;
+}
