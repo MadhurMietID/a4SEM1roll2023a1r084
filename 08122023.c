764b173bc7848479ddd2404d7d7e52e371acc9f4
MadhurMietID <146841949+MadhurMietID@users.noreply.github.com>
2023-12-13 11:24:53 +0530

    Update 08122023.c

--- 08122023.c
+++ 08122023.c
@@ -1,47 +1,31 @@
-//title of program
-//Program to implement an array of structures for student marks for each student and pass it to function for various operations
+//title of the program
+//Program to store the information of student marks using structures and find total marks of individual student
 #include<stdio.h>
 //function heading
-int main()
+void main()
 {
 //structure variable
-struct student
+struct student 
 {
 //variable declaration
 int m1,m2,m3;
 int tot;
 float avg;
-char r;
 };
-int n;
-//output statement 
-printf("enter number of students");
-//input statement 
-scanf("%d",&n);
-//array of structures
-struct student s[n];
+//structure variable
+struct student s1;
+int i;
 //output statement 
 printf("enter the student marks");
-for(int i=0;i<n;i++)
-{
-scanf("%d%d%d",&s[i].m1,&s[i].m2,&s[i].m3);
-s[i].tot=s[i].m1+s[i].m2+s[i].m3;
-s[i].avg=(float)s[i].tot/3.0;
-if(s[i].avg>50)
-s[i].r='P';
-else
-s[i].r='F';
-}
-for(int i=0;i<n;i++)
-{
+//input statement 
+scanf("%d%d%d",&s1.m1,&s1.m2,&s1.m3);
+s1.tot=s1.m1+s1.m2+s1.m3;
+s1.avg=(float)s1.tot/3.0;
 //output statements
-printf("student marklist\n");
-printf("subject1:%d\n",s[i].m1);
-printf("subject2:%d\n",s[i].m2);
-printf("subject3:%d\n",s[i].m3);
-printf("total:%d\n",s[i].tot);
-printf("average:%f\n",s[i].avg);
-printf("result:%c\n",s[i].r);
-}
-return 0;
+printf("student marklist");
+printf("subject1:%d\n",s1.m1);
+printf("subject2:%d\n",s1.m2);
+printf("subject3:%d\n",s1.m3);
+printf("total:%d\n",s1.tot);
+printf("average:%f\n",s1.avg);
 }
